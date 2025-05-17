eb390db5e0dfa57fe4cbd88be4d9a91f1209e1f2
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-05-17 17:04:00 +0200

    added click to move

--- DOTS_RTS_Course/Assets/Scripts/Systems/UnitMoverSystem.cs
+++ DOTS_RTS_Course/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -8,21 +8,17 @@ partial struct UnitMoverSystem : ISystem {
     
     [BurstCompile]
     public void OnUpdate(ref SystemState state) {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed, RefRW<PhysicsVelocity> physicsVelocity)
-                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>()) {
+        foreach ((RefRW<LocalTransform> localTransform, RefRO<UnitMover> unitMover, RefRW<PhysicsVelocity> physicsVelocity)
+                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<UnitMover>, RefRW<PhysicsVelocity>>()) {
 
-            float3 targetPosition = MouseWorldPosition.Instance.GetPosition();
-            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
-
+            float3 moveDirection = unitMover.ValueRO.targetPosition - localTransform.ValueRO.Position;
             moveDirection = math.normalize(moveDirection);
-
-            float rotationSpeed = 10f;
+            
             localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(moveDirection, math.up()),
-                SystemAPI.Time.DeltaTime * rotationSpeed);
+                SystemAPI.Time.DeltaTime * unitMover.ValueRO.rotationSpeed);
 
-            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value;
+            physicsVelocity.ValueRW.Linear = moveDirection * unitMover.ValueRO.moveSpeed;
             physicsVelocity.ValueRW.Angular = float3.zero;
-            // localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
         }
     }
 }
