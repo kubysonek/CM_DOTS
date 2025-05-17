9e640457ca70e35d2d5aadf9eeb1872694c4c1d7
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-05-17 15:44:06 +0200

    converted mover to be physics based

--- DOTS_RTS_Course/Assets/Scripts/Systems/UnitMoverSystem.cs
+++ DOTS_RTS_Course/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -1,17 +1,26 @@
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 
 partial struct UnitMoverSystem : ISystem {
     
     [BurstCompile]
     public void OnUpdate(ref SystemState state) {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed)
-                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>>()) {
-            
-            localTransform.ValueRW.Position =
-                localTransform.ValueRO.Position + new float3(moveSpeed.ValueRO.value, 0, 0) * SystemAPI.Time.DeltaTime;
+        foreach ((RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed, RefRW<PhysicsVelocity> physicsVelocity)
+                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>()) {
+
+            float3 targetPosition = localTransform.ValueRO.Position + new float3(10, 0, 0);
+            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+
+            moveDirection = math.normalize(moveDirection);
+
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value;
+            physicsVelocity.ValueRW.Angular = float3.zero;
+            // localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
         }
     }
 }
