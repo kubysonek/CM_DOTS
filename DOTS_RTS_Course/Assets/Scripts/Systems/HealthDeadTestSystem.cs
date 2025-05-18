cb181b7c17f767de13b617e817d68ac0f5520bda
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-05-18 18:49:26 +0200

    added health system and destroying dead units

---
+++ DOTS_RTS_Course/Assets/Scripts/Systems/HealthDeadTestSystem.cs
@@ -0,0 +1,20 @@
+using Unity.Burst;
+using Unity.Entities;
+
+[UpdateInGroup(typeof(LateSimulationSystemGroup))]
+partial struct HealthDeadTestSystem : ISystem {
+    
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state) {
+        EntityCommandBuffer entityCommandBuffer =
+            SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged);
+
+        foreach ((RefRO<Health> health, Entity entity) in SystemAPI.Query<RefRO<Health>>().WithEntityAccess()) {
+            if (health.ValueRO.healthAmount <= 0) {
+                // This entity is dead
+                entityCommandBuffer.DestroyEntity(entity);
+            }
+        }
+    }
+}
